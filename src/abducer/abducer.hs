ee969cee71728fcb7eb739953f933905b78f1c09
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2009-11-01 14:45:57 -0500

    Added statistics output for abducer.

--- src/abducer/abducer.hs
+++ src/abducer/abducer.hs
@@ -32,8 +32,12 @@ waitForCommands s world = do
              case cameraDetections of
                Left str    -> do { putStrLn str ; return () }
                Right cdets -> do
+                            putStrLn "Got detections."
                             let world' = runAbducer cdets world
+                            logStatistics world world'
+                            putStrLn "Responding with results."
                             respondWithResults s world'
+                            putStrLn "Waiting for command."
                             waitForCommands s world'
     CmdQuit          -> do { return () }
     _                -> do { sleep 500 ; waitForCommands s world }
@@ -42,6 +46,28 @@ getCameraDetections :: Socket -> IO (Either String CameraDetections)
 getCameraDetections s = do
   xml <- getResults s
   return (fromXml $ xmlParse "stream" xml)
+
+logStatistics :: World -> World -> IO ()
+logStatistics world world' =
+    let (Results dets movs paths)    = buildResults world
+        (Results dets' movs' paths') = buildResults world'
+        detsDiff  = (length dets') - (length dets)
+        movsDiff  = (length movs') - (length movs)
+        pathsDiff = (length paths') - (length paths)
+    in do
+      putStr "Hypotheses: "
+      putStr (show $ length dets')
+      putStr " ("
+      putStr (if (detsDiff > 0) then ("+" ++ (show detsDiff)) else (show detsDiff))
+      putStr ") detections, "
+      putStr (show $ length movs')
+      putStr " ("
+      putStr (if (movsDiff > 0) then ("+" ++ (show movsDiff)) else (show movsDiff))
+      putStr ") movements, "
+      putStr (show $ length paths')
+      putStr " ("
+      putStr (if (pathsDiff > 0) then ("+" ++ (show pathsDiff)) else (show pathsDiff))
+      putStrLn ") paths"
 
 respondWithResults :: Socket -> World -> IO ()
 respondWithResults s world = sendResults s $ outputLog world
