bad15e6b03b4c25d440a8b3162baf779cf3d2bda
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2009-11-06 11:51:15 -0500

    Removed absolute paths from Doxyfile, removed beeping from abducer.bat.

--- abducer.bat
+++ abducer.bat
@@ -1,12 +1,12 @@
 :begin
 mkdir build
 
-ghc -O -threaded --make -package HaXml -o build\abducer.exe -isrc\abducer src\abducer\abducer.hs
+ghc -cpp -O -threaded --make -o build\abducer.exe -isrc\abducer src\abducer\abducer.hs
 
 if errorlevel 1 goto run else goto end
 
 :run
-cmd /c build\abducer.exe +RTS -N2 -H256M -M512M -B -RTS
+cmd /c build\abducer.exe +RTS -N2 -RTS
 
 :end
 
