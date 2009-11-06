590375987efae7984d96c2d172881dc77b9a3f08
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2009-11-05 19:34:37 -0500

    Do not start abducer if there is a build error.

--- abducer.bat
+++ abducer.bat
@@ -1,3 +1,3 @@
 mkdir build
-ghc --make -package HaXml -o build\abducer.exe -isrc\abducer src\abducer\abducer.hs
-build\abducer.exe
+
+ghc -O -threaded --make -package HaXml -o build\abducer.exe -isrc\abducer src\abducer\abducer.hs && build\abducer.exe +RTS -N2 -H256M -M512M -B -RTS
